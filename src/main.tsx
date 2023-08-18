9de1a79828827c08aa8bc1e3a34c2f9dc6ade2f4
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-18 11:05:36 +0200

    Setting up React Query DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
@@ -10,6 +11,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {/* So it only goes in my development build, and later when I build for production, ReactQueryDevTools is not going to be included. */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
