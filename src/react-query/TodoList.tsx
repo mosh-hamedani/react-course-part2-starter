129173494998bc0d655da1264df9ddc403cd6c53
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-21 18:58:29 +0200

    Delete useData hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -3,7 +3,7 @@ import useTodos from "./hooks/useTodos";
 const TodoList = () => {
   const { data: todos, error, isLoading } = useTodos();
   // Now this component only has single responsibility is purely concerned with markup. It doesn't know how data should be fetched.
-  // The other benefit is that if tomorrow I need access to these data in another component, I should only call this new hook in tat component. So I use the concept of reusability and modularity.
+  // The other benefit is that if tomorrow I need access to these data in another component, I should only call this new hook in that component. So I use the concept of reusability and modularity.
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
