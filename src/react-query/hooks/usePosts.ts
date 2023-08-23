0dded6473dcd51440b76a7cbf065092822c709a1
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-23 13:42:27 +0200

    Bug fix: page is jumping when click pagination

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -28,6 +28,8 @@ const usePosts = (query: PostQuery) => {
     queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 1 * 60 * 1000, //1m
+    // With setting keepPreviousData property to true, I have a nicer UX as user goes to the next page, there is no jumping up and down. As we go in and out of the loading state, the screen jump up and down. I can improve the UX by keeping the data from current page while the user is waiting for new data. When the new data is available, I can seamless swap the data for the current page ==>
+    keepPreviousData: true,
   });
 };
 export default usePosts;
