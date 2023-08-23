310e40a44b792bcc89c5ca3f869cb4f4093472af
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-23 12:08:53 +0200

    Parameterized queries- add query params and fetch specific data.

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,18 +1,40 @@
 import usePosts from "./hooks/usePosts";
+import { useState } from "react";
 
 const PostList = () => {
-  const { data: posts, error, isLoading } = usePosts();
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts(userId);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
   return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        className="form-select mb-3"
+        //I got the compilation error that said "Argument of type 'string' is not assignable to parameter of type 'SetStateAction number | undefined'." It happened because the value is string but userId is number. So I only should use parseInt to transform it to number type.
+        onChange={(event) => setUserId(parseInt(event.target.value))}
+        // I also should set value prop to userId to have single source of truth
+        value={userId}
+      >
+        <option value="" key=""></option>
+        <option value="1" key="">
+          User 1
+        </option>
+        <option value="2" key="">
+          User 2
+        </option>
+        <option value="3" key="">
+          User 3
+        </option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -6,14 +6,21 @@ interface Post {
   body: string;
   userId: number;
 }
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
   const fetchPosts = () =>
     axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          userId,
+        },
+      })
       .then((res) => res.data);
 
   return useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    // The queryKey is exactly the same as ==> /users/1/posts
+    // Moreover, anytime the userId changes our query will get re-executed(like dependency array in useEffect hook)
+
+    queryKey: ["users", userId, "posts"],
     queryFn: fetchPosts,
     staleTime: 1 * 60 * 1000, //1m
   });
