4fdba9fb777fa307a722a6b9169f71ad88c5eda0
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-23 13:03:48 +0200

    Paginated queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,31 +2,14 @@ import usePosts from "./hooks/usePosts";
 import { useState } from "react";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
   return (
     <>
-      <select
-        className="form-select mb-3"
-        //I got the compilation error that said "Argument of type 'string' is not assignable to parameter of type 'SetStateAction number | undefined'." It happened because the value is string but userId is number. So I only should use parseInt to transform it to number type.
-        onChange={(event) => setUserId(parseInt(event.target.value))}
-        // I also should set value prop to userId to have single source of truth
-        value={userId}
-      >
-        <option value="" key=""></option>
-        <option value="1" key="">
-          User 1
-        </option>
-        <option value="2" key="">
-          User 2
-        </option>
-        <option value="3" key="">
-          User 3
-        </option>
-      </select>
       <ul className="list-group">
         {posts?.map((post) => (
           <li key={post.id} className="list-group-item">
@@ -34,6 +17,19 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        disabled={page === 1}
+        className="btn btn-primary my-3"
+        onClick={() => setPage(page - 1)}
+      >
+        Previous
+      </button>
+      <button
+        className="btn btn-primary my-3 ms-1 "
+        onClick={() => setPage(page + 1)}
+      >
+        Next
+      </button>
     </>
   );
 };
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -6,12 +6,17 @@ interface Post {
   body: string;
   userId: number;
 }
-const usePosts = (userId: number | undefined) => {
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+const usePosts = (query: PostQuery) => {
   const fetchPosts = () =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: {
-          userId,
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
@@ -20,7 +25,7 @@ const usePosts = (userId: number | undefined) => {
     // The queryKey is exactly the same as ==> /users/1/posts
     // Moreover, anytime the userId changes our query will get re-executed(like dependency array in useEffect hook)
     // there is a point, IF I CHECK THE QueryDEVTOOLS I got null when all users selected, but it is ugly. I could fix it very easy
-    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 1 * 60 * 1000, //1m
   });
