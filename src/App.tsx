8017c829be4b0fee4014a2ea52cdb19211027221
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-09-04 21:01:19 +0200

    Create complex action in reducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
-  return <Counter />;
+  return <TaskList />;
 }
 
 export default App;
---
+++ src/state-management/reducers/tasksReducer.ts
@@ -0,0 +1,30 @@
+interface Task {
+  id: number;
+  title: string;
+}
+
+//   So now each task interface has a payload
+// In this case the payload is a task object
+interface AddTask {
+  type: "ADD";
+  task: Task;
+}
+// Here the payload is a task id
+interface DeleteTask {
+  type: "DELETE";
+  taskId: number;
+}
+// Now I define a type, called TaskAction, which is a union of these interfaces
+type TaskAction = AddTask | DeleteTask;
+
+// As a BP I should annotate this reducer with the return type. So, if I make a mistake the TS compiler will let me know.
+const tasksReducer = (tasks: Task[], action: TaskAction): Task[] => {
+  switch (action.type) {
+    case "ADD":
+      return [action.task, ...tasks];
+    case "DELETE":
+      return tasks.filter((t) => t.id !== action.taskId);
+  }
+};
+
+export default tasksReducer;
