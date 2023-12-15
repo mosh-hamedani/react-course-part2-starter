18ce4319b7bc7a8d1e9bd07a8256acc8b697ff39
hykhan94 <hykhan94@gmail.com>
2023-12-15 06:02:52 +0500

    Mutating Data and Handling Errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <PostList />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   userId: number;
