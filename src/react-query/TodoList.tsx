3b629568d10f94378f4c41f334e3f5c5a01e5e21
hykhan94 <hykhan94@gmail.com>
2024-01-11 04:04:25 +0500

    Showing a loading indicator

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -14,11 +14,16 @@ const TodoList = () => {
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
 
-  const { data: todos, error } = useQuery<Todo[], Error>({
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
 
+  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
