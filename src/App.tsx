595f034e0d0d76a6c9591daa41c56c3e5c1f2453
hykhan94 <hykhan94@gmail.com>
2024-01-11 07:31:33 +0500

    Parametrized Queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <>
-      <TodoList />
+      <PostList />
     </>
   );
 }
---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = () => {
+  return useQuery<Post[], Error>({
+    queryKey: ["posts"],
+    queryFn: () =>
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.data),
+    staleTime: 1 * 60 * 1000, //1 minute
+  });
+};
+
+export default usePosts;
