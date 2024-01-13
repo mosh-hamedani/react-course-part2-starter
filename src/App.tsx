9e23acade6d51addcec1ccc8638a35b90164df03
hykhan94 <hykhan94@gmail.com>
2024-01-13 06:14:51 +0500

    Mutating Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <>
-      <PostList />
+      <TodoForm />
+      <TodoList />
     </>
   );
 }
--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,35 +1,47 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "../hooks/useTodos";
+import axios from "axios";
 
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
-
-const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
+const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueriesData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
+  const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <ul className="list-group">
-      {todos.map((todo) => (
-        <li key={todo.id} className="list-group-item">
-          {todo.title}
-        </li>
-      ))}
-    </ul>
+    <form
+      className="row mb-3"
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current && ref.current.value) {
+          addTodo.mutate({
+            id: 0,
+            title: ref.current?.value,
+            userId: 1,
+            completed: false,
+          });
+        }
+      }}
+    >
+      <div className="col">
+        <input ref={ref} type="text" className="form-control" />
+      </div>
+      <div className="col">
+        <button className="btn btn-primary">Add</button>
+      </div>
+    </form>
   );
 };
 
-export default TodoList;
+export default TodoForm;
