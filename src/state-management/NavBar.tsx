80fbf70972e370d56a0501a0a23330617cad7ab7
hykhan94 <hykhan94@gmail.com>
2024-01-23 04:39:19 +0500

    Preventing Unnecessary Renders with Selector

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -5,7 +5,8 @@ import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+  console.log("Render NavBar");
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,14 +2,16 @@ import { create } from "zustand";
 
 interface CounterStore {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
 
 const useCounterStore = create<CounterStore>((set) => ({
   counter: 0,
+  max: 5,
   increment: () => set((store) => ({ counter: store.counter + 1 })),
-  reset: () => set(() => ({ counter: 0 })),
+  reset: () => set(() => ({ max: 10 })),
 }));
 
 export default useCounterStore;
