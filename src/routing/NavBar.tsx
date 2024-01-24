de53df11446312b2934c3ed146a45aa92aedaccd
hykhan94 <hykhan94@gmail.com>
2024-01-24 23:10:45 +0500

    Styling Active Link

--- src/routing/NavBar.tsx
+++ src/routing/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -13,14 +13,14 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link to="/" className="nav-link">
+              <NavLink to="/" className="nav-link">
                 Home
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/users" className="nav-link">
+              <NavLink to="/users" className="nav-link">
                 Users
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </div>
