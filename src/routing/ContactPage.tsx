c47a9a71e2524bf6b4fecec4d5ac1d4a0fbbf81d
hykhan94 <hykhan94@gmail.com>
2024-01-24 05:00:33 +0500

    Navigate

--- src/routing/ContactPage.tsx
+++ src/routing/ContactPage.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const ContactPage = () => {
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         // Redirect the user to the home page
+        navigate("/");
       }}
     >
       <button className="btn btn-primary">Submit</button>
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
+import ContactPage from "./ContactPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
+  { path: "/contact", element: <ContactPage /> },
 ]);
 
 export default router;
