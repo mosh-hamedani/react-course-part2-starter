d5337c69e54786bcbdf28209edd0abe3f03a9010
hykhan94 <hykhan94@gmail.com>
2024-01-24 07:05:37 +0500

    Getting data about current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,10 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(location);
   return <p>User</p>;
 };
 
