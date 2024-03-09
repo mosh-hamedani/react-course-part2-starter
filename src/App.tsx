f86e93c66d77444af1ca784e6c1ae682c05f5f49
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-10 02:05:04 +0800

    add custom hook for fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
+import TodoList from './react-query/TodoList';
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return <TodoList />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
 
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  	document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
   </React.StrictMode>
 );
