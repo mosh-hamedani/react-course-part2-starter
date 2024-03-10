f47ea69599c523d76a4f1348ad5eeb9a4bf02c54
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-10 11:20:06 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
 
@@ -14,6 +15,7 @@ ReactDOM.createRoot(
   <React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<App />
+			<ReactQueryDevtools/>
 		</QueryClientProvider>
   </React.StrictMode>
 );
