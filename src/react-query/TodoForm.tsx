1a0d768d35b8e4ee230dfdfdde71a020331063f4
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-11 14:44:06 +0800

    adding mutation error

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -5,40 +5,53 @@ import axios from 'axios';
 
 const TodoForm = () => {
 	const queryClient = useQueryClient();
-	const addTodo = useMutation({
-		mutationFn:( todo : Todo)=>
+
+	const addTodo = useMutation<Todo, Error, Todo>({
+		mutationFn:(todo: Todo)=>
 			axios
 				.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
 				.then(res => res.data),
+
 		onSuccess: (savedTodo, newTodo )=>{
 			console.log(savedTodo)
 			// Invalidating the cache
-			queryClient.invalidateQueries({
-				queryKey: ["todos"]
-			})
+			// queryClient.invalidateQueries({
+			// 	queryKey: ["todos"]
+			// })
+
+			// Updating the data in the cache
+			queryClient.setQueriesData<Todo[]>(["todos"], todos => [savedTodo, ...(todos || [])] )
 		}
 	})
   	const ref = useRef<HTMLInputElement>(null);
 
 	return (
-		<form className="row mb-3" onSubmit={event =>{
-			event.preventDefault();
+		<>
+			{
+				addTodo.error && (
+				<div className="alert alert-danger">
+					{addTodo.error.message}
+				</div>
+			)}
+			<form className="row mb-3" onSubmit={event =>{
+				event.preventDefault();
 
-			if (ref.current && ref.current?.value)
-			addTodo.mutate({
-				id: 0,
-				title: ref.current?.value,
-				completed: false,
-				userId: 1
-			})
-		}}>
-			<div className="col">
-				<input ref={ref} type="text" className="form-control" />
-			</div>
-			<div className="col">
-				<button className="btn btn-primary">Add</button>
-			</div>
-		</form>
+				if (ref.current && ref.current.value)
+				addTodo.mutate({
+					id: 0,
+					title: ref.current?.value,
+					completed: false,
+					userId: 1
+				})
+			}}>
+				<div className="col">
+					<input ref={ref} type="text" className="form-control" />
+				</div>
+				<div className="col">
+					<button className="btn btn-primary">Add</button>
+				</div>
+			</form>
+		</>
 	);
 };
 
