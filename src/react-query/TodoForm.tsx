64a5834c25b5e0b2b3f283fe4adc8b251b11e49c
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-11 18:46:09 +0800

    Optimistic updates

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,28 +3,46 @@ import { useRef } from 'react';
 import { Todo } from './hooks/useTodos';
 import axios from 'axios';
 
+interface AddTodoContext{
+	previousTodos: Todo[]
+}
+
 const TodoForm = () => {
 	const queryClient = useQueryClient();
 
-	const addTodo = useMutation<Todo, Error, Todo>({
-		mutationFn:(todo: Todo)=>
+	const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
+		mutationFn: (todo: Todo) =>
 			axios
 				.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
-				.then(res => res.data),
-
-		onSuccess: (savedTodo, newTodo )=>{
-			console.log(savedTodo)
+				.then((res) => res.data),
+				
+		onMutate: (newTodo: Todo) => {
 			// Invalidating the cache
 			// queryClient.invalidateQueries({
 			// 	queryKey: ["todos"]
 			// })
+			const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
 
 			// Updating the data in the cache
-			queryClient.setQueriesData<Todo[]>(["todos"], todos => [savedTodo, ...(todos || [])] );
+			queryClient.setQueryData<Todo[]>(["todos"], todos => [newTodo, ...(todos || [])] );
+
 			if(ref.current) ref.current.value = "";
+
+			return { previousTodos }; 
+		},
+		onSuccess: (savedTodo, newTodo) => {
+			queryClient.setQueryData<Todo[]>(["todos"], todos => todos?.map(todo => 
+					(todo === newTodo) ? savedTodo : todo
+				)
+			);
+		},
+		onError: (error, newTodo, context) => {
+			if(!context) return;
+			
+			queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
 		}
-	})
-  	const ref = useRef<HTMLInputElement>(null);
+	});
+	const ref = useRef<HTMLInputElement>(null);
 
 	return (
 		<>
@@ -34,16 +52,16 @@ const TodoForm = () => {
 					{addTodo.error.message}
 				</div>
 			)}
-			<form className="row mb-3" onSubmit={event =>{
+			<form className="row mb-3" onSubmit={event => {
 				event.preventDefault();
 
 				if (ref.current && ref.current.value)
-				addTodo.mutate({
-					id: 0,
-					title: ref.current?.value,
-					completed: false,
-					userId: 1
-				})
+					addTodo.mutate({
+						id: 0,
+						title: ref.current?.value,
+						completed: false,
+						userId: 1
+					});
 			}}>
 				<div className="col">
 					<input ref={ref} type="text" className="form-control" />
